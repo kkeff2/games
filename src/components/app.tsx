399556dbb5fd6693865be327e0e0ada41e33d573
Kasper Bartholdi Gustavii <kasper@kivra.com>
2019-06-19 23:46:43 +0200

    started adding react router

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./app.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { GameDecider } from "./gameDecider";
 import { NumbersGame } from "./numbersGame";
 
@@ -12,11 +13,27 @@ export const App = () => {
     return game === "numbers" ? <NumbersGame /> : <div>LETTERS WIP</div>;
   };
   return (
-    <div className="App">
-      <div className="App-header" onClick={() => setGame(null)}>
-        WELCOME
+    <BrowserRouter>
+      <div className="App">
+        <div className="App-header" onClick={() => setGame(null)}>
+          WELCOME
+        </div>
+        <div className="App-content">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={}
+            />
+            <Route path="/auth/kivra/return" component={CustomRedirect} />
+            <PrivateRoute
+              path="/:agreementKey/"
+              component={AgreementPageContainer}
+            />
+          </Switch>
+          {getContentElement()}
+        </div>
       </div>
-      <div className="App-content">{getContentElement()}</div>
-    </div>
+    </BrowserRouter>
   );
 };
